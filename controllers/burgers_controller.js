f76ee11f23494cca274e1c84f1ffdfc00b520093
Zach Drake <zadrake@yahoo.com>
2019-04-24 19:08:50 -0700

    burger_controller.js added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,8 +5,37 @@ var router = express.Router();
 var burger = require('../models/burger.js');
 
 router.get("/", (req, res) => {
-    burger.all((data) => {
-        
+    burger.selectAll((data) => {
+        var handlebars = {
+            burger: data
+        };
+        console.log(handlebars);
+        res.render("index", handlebars);
     });
 });
+
+router.post("/api/burgers", (req, res) => {
+    burger.insertOne([
+        "burger"
+    ], [
+        req.body.burger_name
+    ], (result) => {
+        res.json({ id: result.inserId });
+    });
+});
+
+router.put("/api/burgers/:id", (req, res) =>{
+    var id = req.params.id;
+    var devoured = req.params.devoured;
+
+    burger.updateOne({ devoured }, id, (result) => {
+        if (result.changedRows == 0) {
+            return res.status(404).end;
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
 
